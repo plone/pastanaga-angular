d72131a6d37c8eb72a1203aa30fd8c2cf5c793f3
Eric BREHAULT <ebrehault@gmail.com>
2017-10-21 12:37:28 +0200

    test app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,69 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { Component } from '@angular/core';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RESTAPIModule } from '@plone/restapi-angular';
+import {
+  MatListModule,
+  MatIconModule
+} from '@angular/material';
+import { IconsModule } from './icons/icons.module';
 
 import { AppComponent } from './app.component';
 
+@Component({
+  selector: 'app-pastanaga-toolbar',
+  template: ''
+})
+export class FakeToolbar { }
+
+@Component({
+  selector: 'app-pastanaga-home',
+  template: ''
+})
+export class FakeHome { }
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        FakeToolbar,
+        FakeHome,
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        HttpClientTestingModule,
+        MatListModule,
+        MatIconModule,
+        IconsModule,
+        RESTAPIModule,
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        {
+          provide: 'CONFIGURATION', useValue: {
+            BACKEND_URL: 'http://fake/Plone',
+          }
+        },
+      ],
+    });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [FakeHome],
+      },
+    });
+  });
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
+
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 });
