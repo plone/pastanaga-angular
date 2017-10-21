005f3e9c9792de403b6e7561c2a8edbaeaf2efc3
Davi Lima <davilima6@gmail.com>
2017-10-21 12:35:28 +0200

    Switch from CSS to SCSS

--- src/app/pastanaga-toolbar/pastanaga-toolbar.component.ts
+++ src/app/pastanaga-toolbar/pastanaga-toolbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pastanaga-toolbar',
   templateUrl: './pastanaga-toolbar.component.html',
-  styleUrls: ['./pastanaga-toolbar.component.css']
+  styleUrls: ['./pastanaga-toolbar.component.scss']
 })
 export class PastanagaToolbarComponent implements OnInit {
 
