3353a2ad971c97ce60988baed9c064389426c3f7
Eric BREHAULT <ebrehault@gmail.com>
2017-10-22 15:05:31 +0200

    fix home page test

--- src/app/pastanaga-home/pastanaga-home.component.spec.ts
+++ src/app/pastanaga-home/pastanaga-home.component.spec.ts
@@ -1,5 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { Component } from '@angular/core';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RESTAPIModule } from '@plone/restapi-angular';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatListModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule
+} from '@angular/material';
+import { IconsModule } from '../icons/icons.module';
+
+import { AppComponent } from '../app.component';
+
 import { PastanagaHomeComponent } from './pastanaga-home.component';
 
 describe('PastanagaHomeComponent', () => {
@@ -8,9 +30,30 @@ describe('PastanagaHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PastanagaHomeComponent ]
+      declarations: [PastanagaHomeComponent],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        MatListModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        IconsModule,
+        RESTAPIModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        {
+          provide: 'CONFIGURATION', useValue: {
+            BACKEND_URL: 'http://fake/Plone',
+          }
+        },
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
