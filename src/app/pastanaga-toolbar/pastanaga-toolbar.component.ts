3d8dcd6b7c0c27a29bf8371d59944f6f865e04d5
Eric BREHAULT <ebrehault@gmail.com>
2017-10-22 18:07:42 +0200

    hide/show toolbar buttons according context

--- src/app/pastanaga-toolbar/pastanaga-toolbar.component.ts
+++ src/app/pastanaga-toolbar/pastanaga-toolbar.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
-import { Services } from '@plone/restapi-angular';
+import { Component, OnInit } from '@angular/core';
+import { Services, TraversingComponent } from '@plone/restapi-angular';
 
 @Component({
   selector: 'app-pastanaga-toolbar',
   templateUrl: './pastanaga-toolbar.component.html',
   styleUrls: ['./pastanaga-toolbar.component.scss']
 })
-export class PastanagaToolbarComponent {
+export class PastanagaToolbarComponent extends TraversingComponent {
   authenticated: boolean;
   collapsed = true;
+  isDocument = false;
+  isFolderish = false;
 
   constructor(public services: Services) {
+    super(services);
     this.services.authentication.isAuthenticated.subscribe(auth => {
       this.authenticated = auth.state;
     });
+  }
+
+  onTraverse(target) {
+    this.isDocument = target.context['@type'] === 'Document';
+    if (target.context.items) {
+      this.isFolderish = true;
+    } else {
+      this.isFolderish = false;
+    }
   }
 
   toggle() {
