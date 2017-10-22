6f9288cdaa076f7b75f6f600ae05495241b087de
Peter Holzer <peter.holzer@agitator.com>
2017-10-22 13:00:43 +0200

    fix login page, make it conform to the basic login view implementation

--- src/app/pastanaga-login/pastanaga-login.component.ts
+++ src/app/pastanaga-login/pastanaga-login.component.ts
@@ -1,36 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { Services, LoginView } from '@plone/restapi-angular';
 
-import { AuthenticationService } from '@plone/restapi-angular';
 
 @Component({
   selector: 'app-pastanaga-login',
   templateUrl: './pastanaga-login.component.html',
   styleUrls: ['./pastanaga-login.component.scss']
 })
-export class PastanagaLoginComponent implements OnInit {
+export class PastanagaLoginComponent extends LoginView {
 
-  constructor(private _authService: AuthenticationService, private _location: Location) {
+  cancel() {
+    this.services.traverser.traverse('/');
+    return false;
   }
 
-  ngOnInit() {
+  logout() {
+    this.services.authentication.logout();
+    return false;
   }
-
-  login(event: Event, value: Object = {}, form) {
-    event.preventDefault();
-    this._authService.login(form.username, form.password);
-  }
-
-  logout(event) {
-    event.preventDefault();
-    this._authService.logout();
-  }
-
-  goBack(event) {
-    event.preventDefault();
-    this._location.back();
-  }
-
 }
