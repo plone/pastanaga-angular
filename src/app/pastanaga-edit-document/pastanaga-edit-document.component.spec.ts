1dfd4a15ca03ae5fc710721656ac565799dff1d9
Sune Broendum Woeller <sune@woeller.dk>
2017-10-22 10:24:42 +0200

    fix the test imports

--- src/app/pastanaga-edit-document/pastanaga-edit-document.component.spec.ts
+++ src/app/pastanaga-edit-document/pastanaga-edit-document.component.spec.ts
@@ -1,5 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { Component } from '@angular/core';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RESTAPIModule } from '@plone/restapi-angular';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatListModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule
+} from '@angular/material';
+import { IconsModule } from '../icons/icons.module';
+
+import { AppComponent } from '../app.component';
+
 import { PastanagaEditDocumentComponent } from './pastanaga-edit-document.component';
 
 describe('PastanagaEditDocumentComponent', () => {
@@ -8,7 +30,28 @@ describe('PastanagaEditDocumentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PastanagaEditDocumentComponent ]
+      declarations: [ PastanagaEditDocumentComponent ],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        MatListModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        IconsModule,
+        RESTAPIModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        {
+          provide: 'CONFIGURATION', useValue: {
+            BACKEND_URL: 'http://fake/Plone',
+          }
+        },
+      ],
     })
     .compileComponents();
   }));
