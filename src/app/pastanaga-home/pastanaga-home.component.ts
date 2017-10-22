dc1ef63223db63e0ce05de3f9e01bac2de740322
Eric BREHAULT <ebrehault@gmail.com>
2017-10-22 14:43:32 +0200

    add login link

--- src/app/pastanaga-home/pastanaga-home.component.ts
+++ src/app/pastanaga-home/pastanaga-home.component.ts
@@ -9,12 +9,16 @@ import { Services } from '@plone/restapi-angular';
 export class PastanagaHomeComponent implements OnInit {
 
   documents: any[] = [];
+  authenticated = false;
 
   constructor(public services: Services) { }
 
   ngOnInit() {
     this.services.resource.find({ portal_type: 'Document' }).subscribe(res => {
       this.documents = res.items;
+    });
+    this.services.authentication.isAuthenticated.subscribe(auth => {
+      this.authenticated = auth.state;
     });
   }
 
