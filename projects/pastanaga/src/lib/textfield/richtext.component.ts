a17757b290ccab7834450a5bac097129e145e49c
Ramon Navarro Bosch <ramon.nb@gmail.com>
2018-09-26 15:56:37 +0200

    Trying to make it work on nsf-pastanaga

--- projects/pastanaga/src/lib/textfield/richtext.component.ts
+++ projects/pastanaga/src/lib/textfield/richtext.component.ts
@@ -13,18 +13,18 @@ import {
 import * as MediumEditor from 'medium-editor';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+let nextId = 0;
 
 @Component({
   selector: 'pa-richtext',
   templateUrl: 'richtext.component.html',
   styleUrls: ['textfield.scss', 'richtext.component.scss'],
 })
-export class RichtextComponent implements ControlValueAccessor, OnInit, OnChanges, OnDestroy, AfterViewInit {
+export class RichtextComponent implements ControlValueAccessor, OnInit, OnDestroy {
 
   private lastViewModel: string;
   private editor: any;
   private active: boolean;
-  private htmlContent: string;
   @Input() id: string;
   @Input() name: string;
   @Input() value = '';
@@ -46,44 +46,111 @@ export class RichtextComponent implements ControlValueAccessor, OnInit, OnChange
       pattern: false,
   };
 
-  @Input('editorModel') model: any;
   @Input('editorOptions') options: any;
 
   constructor() { }
 
   ngOnInit() {
     this.active = true;
+    if (!this.id) {
+      this.id = `richtext-${nextId++}`;
+    }
+    this.name = this.name || this.id;
+    if (this.help) {
+        this.helpId = `${this.id}-help`;
+    }
 
     if (this.placeholder && this.placeholder.length) {
       this.options.placeholder = {
         text : this.placeholder
       };
     }
-  }
 
-  ngAfterViewInit() {
+    if (this.isDisabled || this.isReadOnly) {
+      this.options.disableEditing = true;
+    }
+
+    this.options.anchorPreview = {
+      /* These are the default options for anchor preview,
+         if nothing is passed this is what it used */
+      hideDelay: 500,
+      previewValueSelector: 'a'
+    }
+
+    this.options.anchor = {
+      /* These are the default options for anchor form,
+         if nothing is passed this is what it used */
+      customClassOption: null,
+      customClassOptionText: 'Button',
+      linkValidation: false,
+      placeholderText: 'Paste or type a link',
+      targetCheckbox: false,
+      targetCheckboxText: 'Open in new window'
+    }
+
+    this.options.paste= {
+      /* This example includes the default options for paste,
+         if nothing is passed this is what it used */
+      forcePlainText: true,
+      cleanPastedHTML: false,
+      cleanReplacements: [],
+      cleanAttrs: ['class', 'style', 'dir'],
+      cleanTags: ['meta'],
+      unwrapTags: []
+    }
+
+    this.options.keyboardCommands = {
+      /* This example includes the default options for keyboardCommands,
+         if nothing is passed this is what it used */
+      commands: [
+          {
+              command: 'bold',
+              key: 'B',
+              meta: true,
+              shift: false,
+              alt: false
+          },
+          {
+              command: 'italic',
+              key: 'I',
+              meta: true,
+              shift: false,
+              alt: false
+          },
+          {
+              command: 'underline',
+              key: 'U',
+              meta: true,
+              shift: false,
+              alt: false
+          }
+      ],
+    }
+
+    this.options.imageDragging = true;
     this.editor = new MediumEditor('.me-editable', this.options);
-    this.editor.setContent(this.model);
     this.editor.subscribe('editableInput', (event, editable) => {
-      this.updateModel();
+      this.change(event.data)
     });
   }
 
-  refreshView() {
-    if (this.editor) {
-      this.editor.setContent(this.model);
-    }
-  }
 
-  ngOnChanges(changes): void {
-    if (this.isPropertyUpdated(changes, this.lastViewModel)) {
-      this.lastViewModel = this.model;
-      this.refreshView();
-    }
-  }
+  // refreshView() {
+  //   if (this.editor) {
+  //     this.editor.setContent(this.model);
+  //   }
+  // }
+
+  // ngOnChanges(changes): void {
+  //   if (this.isPropertyUpdated(changes, this.lastViewModel)) {
+  //     this.lastViewModel = this.model;
+  //     this.refreshView();
+  //   }
+  // }
 
   writeValue(value: any) {
-    this.lastViewModel = value;
+    debugger;
+    this.editor.setContent(value);
   }
 
   registerOnTouched(handler: any) {
@@ -98,15 +165,27 @@ export class RichtextComponent implements ControlValueAccessor, OnInit, OnChange
       this.isDisabled = isDisabled;
   }
 
+  change(value: any) {
+    let all_text = this.editor.getContent();
+    this.valueChange.emit(all_text);
+    this.keyUp.emit(value);
+    if (this.onChange) {
+        this.onChange(all_text);
+    }
+    if (this.onTouched) {
+        this.onTouched(all_text);
+    }
+  }
+
   /**
    * Emit updated model
    */
-  updateModel(): void {
-    let value = this.editor.getContent();
-    value = value.replace(/&nbsp;/g, '').trim();
-    this.lastViewModel = value;
-    this.valueChange.emit(value);
-  }
+  // updateModel(): void {
+  //   let value = this.editor.getContent();
+  //   value = value.replace(/&nbsp;/g, '').trim();
+  //   this.lastViewModel = value;
+  //   this.valueChange.emit(value);
+  // }
 
   /**
    * Remove MediumEditor on destruction of directive
@@ -115,14 +194,14 @@ export class RichtextComponent implements ControlValueAccessor, OnInit, OnChange
     this.editor.destroy();
   }
 
-  isPropertyUpdated(changes, viewModel) {
-    if (!changes.hasOwnProperty('model')) { return false; }
+  // isPropertyUpdated(changes, viewModel) {
+  //   if (!changes.hasOwnProperty('model')) { return false; }
 
-    const change = changes.model;
+  //   const change = changes.model;
 
-    if (change.isFirstChange()) {
-      return true;
-    }
-    return !ɵlooseIdentical(viewModel, change.currentValue);
-  }
+  //   if (change.isFirstChange()) {
+  //     return true;
+  //   }
+  //   return !ɵlooseIdentical(viewModel, change.currentValue);
+  // }
 }
