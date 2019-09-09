13d2d6db4f0c7a7e56a13ebb9bb52b3e36a6002e
niglesiaszingoni <niglesiaszingoni@gmail.com>
2019-09-09 14:31:01 +0200

    update deps

--- projects/pastanaga/src/lib/translate/translate.directive.ts
+++ projects/pastanaga/src/lib/translate/translate.directive.ts
@@ -2,32 +2,30 @@ import { Directive, ElementRef, Input, AfterViewChecked } from '@angular/core';
 import { TranslatePipe } from './translate.pipe';
 
 @Directive({
-  selector: '[translate]'
+    selector: '[translate]'
 })
 export class TranslateDirective implements AfterViewChecked {
     key = '';
     lastParams: any;
     currentParams: any;
-    constructor(
+    @Input() set translate(key: string) {
+        if (key) {
+            this.key = key;
+            this.checkNodes();
+        }
+    }
+    @Input() set translateParams(params: any) {
+        if (!this.compareObj(this.currentParams, params)) {
+            this.currentParams = params;
+            this.checkNodes(true);
+        }
+    }
+    constructor (
         private eltRef: ElementRef,
         private translatePipe: TranslatePipe
     ) {
     }
 
-    @Input() set translate(key: string) {
-        if (key) {
-          this.key = key;
-          this.checkNodes();
-        }
-    }
-
-    @Input() set translateParams(params: any) {
-        if (!this.compareObj(this.currentParams, params)) {
-          this.currentParams = params;
-          this.checkNodes(true);
-        }
-    }
-    
     ngAfterViewChecked() {
         this.checkNodes();
     }
@@ -36,55 +34,55 @@ export class TranslateDirective implements AfterViewChecked {
         let nodes: NodeList = this.eltRef.nativeElement.childNodes;
         // if the element is empty
         if (!nodes.length) {
-          // we add the key as content
-          this.setContent(this.eltRef.nativeElement, this.key);
-          nodes = this.eltRef.nativeElement.childNodes;
+            // we add the key as content
+            this.setContent(this.eltRef.nativeElement, this.key);
+            nodes = this.eltRef.nativeElement.childNodes;
         }
         for (let i = 0; i < nodes.length; ++i) {
-          let node: any = nodes[i];
-          if (node.nodeType === 3) { // node type 3 is a text node
-            let key = '';
-            if (this.key) {
-              key = this.key;
-              if (forceUpdate) {
-                node.lastKey = null;
-              }
-            } else {
-              let content = this.getContent(node);
-              let trimmedContent = content.trim();
-              if (trimmedContent.length) {
-                if (content !== node.currentValue) {
-                  key = trimmedContent;
-                  node.originalContent = this.getContent(node);
-                } else if (node.originalContent && forceUpdate) {
-                  node.lastKey = null;
-                  key = node.originalContent.trim();
+            let node: any = nodes[i];
+            if (node.nodeType === 3) { // node type 3 is a text node
+                let key = '';
+                if (this.key) {
+                    key = this.key;
+                    if (forceUpdate) {
+                        node.lastKey = null;
+                    }
+                } else {
+                    let content = this.getContent(node);
+                    let trimmedContent = content.trim();
+                    if (trimmedContent.length) {
+                        if (content !== node.currentValue) {
+                            key = trimmedContent;
+                            node.originalContent = this.getContent(node);
+                        } else if (node.originalContent && forceUpdate) {
+                            node.lastKey = null;
+                            key = node.originalContent.trim();
+                        }
+                    }
                 }
-              }
+                this.updateValue(key, node);
             }
-            this.updateValue(key, node);
-          }
         }
     }
 
     updateValue(key: string, node: any) {
         if (key) {
-          if (node.lastKey === key && this.compareObj(this.lastParams, this.currentParams)) {
-            return;
-          }
-    
-          this.lastParams = this.currentParams;
-          
-          const translate = this.translatePipe.transform(key);
-          if (translate !== key) {
-            node.lastKey = key;
-          }
-          if (!node.originalContent) {
-            node.originalContent = this.getContent(node);
-          }
-          node.currentValue = !!translate ? translate : (node.originalContent || key);
-          // we replace in the original content to preserve spaces that we might have trimmed
-          this.setContent(node, this.key ? node.currentValue : node.originalContent.replace(key, node.currentValue));
+            if (node.lastKey === key && this.compareObj(this.lastParams, this.currentParams)) {
+                return;
+            }
+
+            this.lastParams = this.currentParams;
+
+            const translate = this.translatePipe.transform(key);
+            if (translate !== key) {
+                node.lastKey = key;
+            }
+            if (!node.originalContent) {
+                node.originalContent = this.getContent(node);
+            }
+            node.currentValue = !!translate ? translate : (node.originalContent || key);
+            // we replace in the original content to preserve spaces that we might have trimmed
+            this.setContent(node, this.key ? node.currentValue : node.originalContent.replace(key, node.currentValue));
         }
     }
 
@@ -101,10 +99,10 @@ export class TranslateDirective implements AfterViewChecked {
     }
 
     compareObj(obj1: any, obj2: any) {
-        Object.entries(obj1).map(([key, value]) => {
+        Object.entries(obj1).forEach(([key, value]) => {
             let exists = false;
-            Object.entries(obj2).map(([key2, value2]) => {
-                if(key === key2) {
+            Object.entries(obj2).forEach(([key2, value2]) => {
+                if (key === key2) {
                     if (value === value2) {
                         exists = true;
                     }
--- projects/pastanaga/src/lib/translate/translate.pipe.ts
+++ projects/pastanaga/src/lib/translate/translate.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as translateKeys from '../../../../../src/assets/i18n/en.json';
+import { TranslateService } from '@ngx-translate/core';
 @Pipe({
   name: 'translate'
 })
@@ -7,22 +8,24 @@ export class TranslatePipe implements PipeTransform {
     lastKey?: string;
     lastParams?: string;
     value: string = '';
+    constructor(private translateService: TranslateService) {}
     transform(key: string, args?: any): any {
         // if we ask another time for the same key, return the last value
         if (key === this.lastKey && args === this.lastParams) {
             return this.value;
         }
         const keys = !!key ? key.split('.') : [];
-        this.value = translateKeys['default'];
-        keys.map(k => {
-            this.value = this.value[k];
-            if (!this.value) {
-                this.value = '';
+        let value = translateKeys['default'];
+        keys.forEach(k => {
+            value = value[k];
+            if (!value) {
+                value = '';
             }
         });
+        this.value = value;
         if (!!this.value && !!args) {
             this.lastParams = args;
-            Object.keys(args).map(key => {
+            Object.keys(args).forEach(key => {
                 this.value = this.value.replace(`{{${key}}}`, args[key]);
             });
         }
