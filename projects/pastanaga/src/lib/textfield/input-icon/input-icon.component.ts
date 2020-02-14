a25e5014e6316dd48e8f64dcb220c71ecdea1628
Tilda <mathilde@onna.com>
2020-02-14 16:35:22 +0100

    Add icon-input and password input components

---
+++ projects/pastanaga/src/lib/textfield/input-icon/input-icon.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+
+@Component({
+    selector: 'pa-input-icon',
+    templateUrl: './input-icon.component.html',
+    styleUrls: ['./input-icon.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputIconComponent {
+    @Input() iconName?: string;
+    @Input() iconTooltip?: string;
+    @Input() iconColor: 'primary' | 'secondary' | 'destructive' = 'primary';
+    @Input()
+    set disabled(value) { this._disabled = coerceBooleanProperty(value); }
+    get disabled() { return this._disabled; }
+    _disabled = false;
+
+    @Output() iconClick = new EventEmitter<void>();
+
+    onIconClick() {
+        this.iconClick.emit();
+    }
+}
--- projects/pastanaga/src/lib/textfield/textfield.module.ts
+++ projects/pastanaga/src/lib/textfield/textfield.module.ts
@@ -4,21 +4,35 @@ import { InputComponent } from './input.component';
 import { SelectComponent } from './select.component';
 import { TextareaComponent } from './textarea.component';
 import { TranslateModule } from '../translate/translate.module';
+import { SvgModule } from '../svg/svg.module';
+import { PasswordInputComponent } from './password-input/password-input.component';
+import { InputIconComponent } from './input-icon/input-icon.component';
+import { ButtonModule } from '../button/button.module';
+import { TooltipModule } from '../tooltip/tooltip.module';
+import { ProgressModule } from '../progress/progress.module';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
+        SvgModule,
+        ButtonModule,
+        TooltipModule,
+        ProgressModule,
     ],
     declarations: [
         InputComponent,
+        InputIconComponent,
         SelectComponent,
         TextareaComponent,
+        PasswordInputComponent,
     ],
     exports: [
         InputComponent,
+        InputIconComponent,
         SelectComponent,
         TextareaComponent,
+        PasswordInputComponent,
     ],
 })
 export class TextFieldModule {
