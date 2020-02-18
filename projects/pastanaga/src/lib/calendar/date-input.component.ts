81f11dfd815479b4189469caef63ed03cd97e9f3
Raül Clemente Ayats <raul@onna.com>
2020-02-18 09:08:52 +0100

    [ch11930] Refined styles and managing today and yesterday dates

--- projects/pastanaga/src/lib/calendar/date-input.component.ts
+++ projects/pastanaga/src/lib/calendar/date-input.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, Renderer2 } from '@angular/core';
 import { PopupComponent } from '../popup/popup.component';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { PopupService } from '../popup/popup.service';
 
 @Component({
@@ -11,30 +10,13 @@ import { PopupService } from '../popup/popup.service';
 })
 export class DateInputComponent extends PopupComponent {
 
-    dateInput = "";
+    dateInput = '';
+    dateError = false;
+    isValidDate = false;
+    currentDate: Date = new Date();
 
-    @Input() rangeSelection: any;
-
-    @Input() set rangeStart(value) {
-        this._rangeStart = coerceBooleanProperty(value);
-        this._dontCloseOnSelection = true;
-    }
-    _rangeStart = false;
-
-    @Input() set rangeEnd(value) {
-        this._rangeEnd = coerceBooleanProperty(value);
-        this._dontCloseOnSelection = false;
-    }
-    _rangeEnd = false;
-
-    @Input() set dontCloseOnSelection(value) { this._dontCloseOnSelection = coerceBooleanProperty(value); }
-    _dontCloseOnSelection = false;
-
-    @Input() min?: Date;
-    @Input() selection?: {start?: Date, end?: Date} | Date;
-
-    @Input() set noFuture(value) { this._noFuture = coerceBooleanProperty(value); }
-    _noFuture = false;
+    @Input() dateHelp: string = '';
+    @Input() datePlaceholder: string = 'mm/dd/yyyy';
 
     @Output() select: EventEmitter<Date | null> = new EventEmitter<Date>();
 
@@ -47,35 +29,64 @@ export class DateInputComponent extends PopupComponent {
         super(popupService, renderer, element, cdr);
     }
 
-    onSelection(date: Date | null) {
-        if (!this._dontCloseOnSelection) {
-            this.close();
-        }
-        this.select.emit(date);
-    }
-
-    onOutsideClick(event) {
-        if (!this.isNodeFromCalendar(event.target)) {
-            this.close();
-        }
-    }
-
-    private isNodeFromCalendar(node: HTMLElement): boolean {
-        if (typeof node.className === 'string' && node.className.includes('pa-calendar')) {
-            return true;
-        } else if (!!node.parentElement) {
-            return this.isNodeFromCalendar(node.parentElement);
-        } else {
-            return false;
-        }
-    }
-
-    private selectForRange(date: Date) {
+    selectDate(date: Date) {
+        this.currentDate = date;
+        this.dateInput = '';
         const dd = date.getDate();
         const mm = date.getMonth() + 1;
         const yyyy = date.getFullYear();
-        console.log('event: ', date);
-        this.dateInput = dd + '/' + mm + '/' + yyyy;
+        const today = new Date();
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        if (today.getFullYear() === date.getFullYear()){
+            if (today.getMonth() === date.getMonth() && today.getDate() === date.getDate()){
+                this.dateInput = 'Today';
+            } else if (yesterday.getMonth() === date.getMonth() && yesterday.getDate() === date.getDate()){
+                this.dateInput = 'Yesterday';
+            }
+        }
+        if (this.dateInput === '') {
+            this.dateInput = mm + '/' + dd + '/' + yyyy; // American date format
+        }
         this.cdr.markForCheck();
+        this.select.emit(this.currentDate);
+    }
+
+    checkTypedDate(date: string) {
+        if (date !== ''){
+            this.dateError = false;
+            this.isValidDate = true;
+            if (date.toLowerCase() === 'today') {
+                this.currentDate = new Date();
+            } else if (date.toLowerCase() === 'yesterday') {
+                let yesterday = new Date();
+                yesterday.setDate(yesterday.getDate() - 1);
+                this.currentDate = yesterday;
+            } else {
+                const typedDate = new Date(date);
+                this.isValidDate = date.length >= 8 && typedDate.toString() !== 'Invalid Date';
+                if (this.isValidDate) {
+                    const brokenDate = date.split('/');
+                    if (brokenDate[0] === '2' || brokenDate[0] === '02') {
+                        this.isValidDate = this.checkFebruaryDate(brokenDate);
+                    }
+                }
+                this.dateError = !this.isValidDate;
+            }
+            if (this.isValidDate) {
+                this.select.emit(this.currentDate);
+            }
+        }
+    }
+
+    private checkFebruaryDate(brokenDate: string[]) {
+        if (Number(brokenDate[1]) <= 28) {
+            return true;
+        } else if (Number(brokenDate[1]) >= 30) {
+            return false;
+        } else {
+            const year = Number(brokenDate[2]);
+            return (year % 4 === 0) && ((year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0));
+        }
     }
 }
