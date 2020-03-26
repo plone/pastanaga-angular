2a9e308316c8bc48a8c9fc208f2c5327b14f0c31
Nicolas iglesias <niglesiaszingoni@gmail.com>
2020-03-26 12:57:32 +0100

    If value changes we need to update it with the proper value

--- projects/pastanaga/src/lib/textfield/select.component.ts
+++ projects/pastanaga/src/lib/textfield/select.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator } from '@angular/forms';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 let nextId = 0;
 
@@ -17,7 +18,7 @@ let nextId = 0;
         multi: true,
     }],
 })
-export class SelectComponent implements ControlValueAccessor, OnInit, AfterViewInit, Validator {
+export class SelectComponent implements ControlValueAccessor, OnChanges,OnInit, AfterViewInit, Validator {
     @Input() id?: string;
     @Input() name?: string;
     @Input() label?: string;
@@ -26,10 +27,27 @@ export class SelectComponent implements ControlValueAccessor, OnInit, AfterViewI
     @Input() help?: string;
     @Input() errorHelp ?: string;
     @Input() errorMessage ?: string;
-    @Input() disabled = false;
-    @Input() required = false;
-    @Input() isLabelHidden = false;
-    @Input() isLessen = false;
+    @Input()
+    get disabled(): boolean { return this._disabled; }
+    set disabled(value: boolean) { this._disabled = coerceBooleanProperty(value); }
+    @Input()
+    get readOnly(): boolean { return this._readOnly; }
+    set readOnly(value: boolean) { this._readOnly = coerceBooleanProperty(value); }
+    @Input()
+    get required(): boolean { return this._required; }
+    set required(value: boolean) { this._required = coerceBooleanProperty(value); }
+    @Input()
+    get labelHidden(): boolean { return this._labelHidden; }
+    set labelHidden(value: boolean) { this._labelHidden = coerceBooleanProperty(value); }
+    @Input()
+    get lessen(): boolean { return this._lessen; }
+    set lessen(value: boolean) { this._lessen = coerceBooleanProperty(value); }
+    @Input()
+    get isAccent(): boolean { return this._accent; }
+    set isAccent(value: boolean) { this._accent = coerceBooleanProperty(value); }
+    @Input()
+    get selectablePlaceholder(): boolean { return this._selectablePlaceholder; }
+    set selectablePlaceholder(value: boolean) { this._selectablePlaceholder = coerceBooleanProperty(value); }
     @Output() onSelection: EventEmitter<any> = new EventEmitter();
     @Output() valueChange: EventEmitter<any> = new EventEmitter();
     helpId = '';
@@ -37,8 +55,15 @@ export class SelectComponent implements ControlValueAccessor, OnInit, AfterViewI
     onTouched?: Function;
     hasNoSelection = false;
     isPlaceHolderSelected = false;
-    hasError = false;
-
+    hasError = false;    
+    _disabled = false;
+    _readOnly = false;
+    _required = false;
+    _labelHidden = false;
+    _lessen = false;
+    _accent = false;
+    _selectablePlaceholder = false;
+    
     constructor(private element: ElementRef) {
     }
 
@@ -50,20 +75,30 @@ export class SelectComponent implements ControlValueAccessor, OnInit, AfterViewI
         }
     }
 
+    ngOnChanges(changes: SimpleChanges) {
+        if (!!changes.value) {
+            this.setProperValue(changes.value.currentValue);
+        }
+    }
+
     ngAfterViewInit() {
         setTimeout(() => {
-            if (!!this.value) {
-                this.element.nativeElement.querySelector('select').value = this.value;
-                this.isPlaceHolderSelected = this.value === '__PLACEHOLDER__';
-            } else if (!!this.placeholder) {
-                this.element.nativeElement.querySelector('select').value = '__PLACEHOLDER__';
-                this.isPlaceHolderSelected = true;
-            } else {
-                const firstOption = this.element.nativeElement.querySelector('option:first-child');
-                const noOptionSelected = this.element.nativeElement.querySelectorAll('option:checked').length === 0;
-                this.hasNoSelection = noOptionSelected && !!firstOption && !firstOption.innerText;
-            }
+            this.setProperValue(this.value);
         }, 0);
+    }
+
+    setProperValue(value: string) {
+        if (!!value) {
+            this.element.nativeElement.querySelector('select').value = this.value;
+            this.isPlaceHolderSelected = this.value === '__PLACEHOLDER__';
+        } else if (!!this.placeholder) {
+            this.element.nativeElement.querySelector('select').value = '__PLACEHOLDER__';
+            this.isPlaceHolderSelected = true;
+        } else {
+            const firstOption = this.element.nativeElement.querySelector('option:first-child');
+            const noOptionSelected = this.element.nativeElement.querySelectorAll('option:checked').length === 0;
+            this.hasNoSelection = noOptionSelected && !!firstOption && !firstOption.innerText;
+        }
     }
 
     writeValue(value: any) {
