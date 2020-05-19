4459db026a895befbc0584819351d12345dbd311
Eric BREHAULT <ebrehault@gmail.com>
2020-05-19 08:52:11 +0200

    Fix #172 remove traversal dependency

--- projects/pastanaga/src/lib/button/button.module.ts
+++ projects/pastanaga/src/lib/button/button.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { ButtonComponent } from './button.component';
-import { ButtonLinkComponent } from './button-link.component';
 import { SvgModule } from '../svg/svg.module';
-import { TraversalModule } from 'angular-traversal';
 import { TranslateModule } from '../translate/translate.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        TranslateModule,
         SvgModule,
-        RouterModule,
-        TraversalModule,
+        TranslateModule,
     ],
-    declarations: [ButtonComponent, ButtonLinkComponent],
-    exports: [ButtonComponent, ButtonLinkComponent],
+    declarations: [ButtonComponent],
+    exports: [ButtonComponent],
 })
 export class ButtonModule {
 }
