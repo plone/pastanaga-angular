13714e377f1267d3d41e813fd97d36079391ec09
mpellerin <mathilde@onna.com>
2020-06-05 14:37:06 +0200

    Make icon page working from other application

---
+++ projects/demo/src/app/demo/icon-page/base-icon-page.component.ts
@@ -0,0 +1,30 @@
+import { Size } from '../../../../../pastanaga-angular/src';
+
+export class BaseIconPageComponent {
+    icons: string[] = [];
+    selectedSize: Size = Size.medium;
+    selectedColor?: string;
+    selectedBackground?: string;
+    nameResolvedExample = './assets/icons/${name}.svg';
+    codeExample = `<pa-icon [name]="iconName" [size]="selectedSize" [color]="selectedColor" [background]="selectedBackground"></pa-icon>
+
+<pa-icon name="heart"></pa-icon>
+
+<pa-icon path="./assets/dialog/heart-band.png"></pa-icon>
+`;
+
+    constructor() {
+    }
+
+    updateSize(value: string) {
+        this.selectedSize = value as Size;
+    }
+
+    updateColor(value: string) {
+        this.selectedColor = value === 'none' ? undefined : value;
+    }
+
+    updateBackground(value: string) {
+        this.selectedBackground = value === 'none' ? undefined : value;
+    }
+}
--- projects/demo/src/app/demo/icon-page/icon-page.component.ts
+++ projects/demo/src/app/demo/icon-page/icon-page.component.ts
@@ -1,41 +1,17 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+// @ts-ignore
 import ICON_LIST from '../../../icons.json';
-import { Size } from '../../../../../pastanaga-angular/src';
+import { BaseIconPageComponent } from './base-icon-page.component';
 
 @Component({
     templateUrl: './icon-page.component.html',
     styleUrls: ['./icon-page.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IconPageComponent implements OnInit {
-    icons: string[] = [];
-    selectedSize: Size = Size.medium;
-    selectedColor?: string;
-    selectedBackground?: string;
-    nameResolvedExample = './assets/icons/${name}.svg';
-    codeExample = `<pa-icon [name]="iconName" [size]="selectedSize" [color]="selectedColor" [background]="selectedBackground"></pa-icon>
-
-<pa-icon name="heart"></pa-icon>
-
-<pa-icon path="./assets/dialog/heart-band.png"></pa-icon>
-`;
-
-    constructor() {
-    }
+export class IconPageComponent extends BaseIconPageComponent implements OnInit {
 
     ngOnInit(): void {
-        this.icons = ICON_LIST.map(icon => icon.substring(0, icon.lastIndexOf('.')));
+        this.icons = ICON_LIST.map((icon: string) => icon.substring(0, icon.lastIndexOf('.')));
     }
 
-    updateSize(value: string) {
-        this.selectedSize = value as Size;
-    }
-
-    updateColor(value: string) {
-        this.selectedColor = value === 'none' ? undefined : value;
-    }
-
-    updateBackground(value: string) {
-        this.selectedBackground = value === 'none' ? undefined : value;
-    }
 }
