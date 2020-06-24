914c7e144db3f267cdd13f2cc30c1c9e068dc040
Eric BREHAULT <ebrehault@gmail.com>
2020-06-24 12:15:27 +0200

    fix icon size in button

--- projects/pastanaga-angular/src/lib/button/button.component.spec.ts
+++ projects/pastanaga-angular/src/lib/button/button.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaIconModule } from '../icon/icon.module';
 import { ButtonComponent } from './button.component';
+import { Size } from '../common';
 
 describe('ButtonComponent', () => {
     let component: ButtonComponent;
@@ -25,5 +26,11 @@ describe('ButtonComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set the icon size according the button size', () => {
+        expect(component._iconSize).toEqual(Size.medium);
+        component.size = Size.small;
+        expect(component._iconSize).toEqual(Size.small);
     });
 });
