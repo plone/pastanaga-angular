2ba13a3f94ecb1065f847e9513f8914390521a5a
mpellerin <mathilde@onna.com>
2020-07-08 11:48:37 +0200

    setup prettier

--- projects/demo/src/app/demo/pages/icon-page/icon-page.component.ts
+++ projects/demo/src/app/demo/pages/icon-page/icon-page.component.ts
@@ -6,11 +6,10 @@ import { BaseIconPageComponent } from './base-icon-page.component';
 @Component({
     templateUrl: './icon-page.component.html',
     styleUrls: ['./icon-page.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IconPageComponent extends BaseIconPageComponent implements OnInit {
     ngOnInit(): void {
         this.icons = ICON_LIST;
     }
-
 }
