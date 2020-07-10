2ecc16d1025c1f5d1db762af5bc00d98720e0627
mpellerin <mathilde@onna.com>
2020-07-10 16:54:42 +0200

    New color palette

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -19,55 +19,58 @@ import { DateTimePageComponent } from './demo/pages/datetime-page/datetime-page.
 import { TogglePageComponent } from './demo/pages/toggle-page/toggle-page.component';
 import { ToastPageComponent } from './demo/pages/toast-page/toast-page.component';
 import { TooltipPageComponent } from './demo/pages/tooltip-page/tooltip-page.component';
+import { PalettePageComponent } from './demo/pages/palette-page/palette-page.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     menu: IDemoMenuSection[] = [
         {
             title: 'Atoms',
             pages: [
-                {view: 'icon', title: 'Icons', type: IconPageComponent},
+                { view: 'icon', title: 'Icons', type: IconPageComponent },
+                { view: 'palette', title: 'Palette', type: PalettePageComponent },
             ],
         },
         {
             title: 'Components',
             pages: [
-                {view: 'avatar', title: 'Avatar', type: AvatarPageComponent},
-                {view: 'button', title: 'Button', type: ButtonPageComponent},
-                {view: 'checkbox', title: 'Checkbox', type: CheckboxPageComponent},
-                {view: 'datetime', title: 'Date/time', type: DateTimePageComponent},
-                {view: 'dropdown', title: 'Dropdown', type: DropdownPageComponent},
-                {view: 'input', title: 'Input', type: InputPageComponent},
-                {view: 'popup', title: 'Popup', type: PopupPageComponent},
-                {view: 'select', title: 'Select', type: SelectPageComponent},
-                {view: 'tables', title: 'Tables', type: TablePageComponent},
-                {view: 'tabs', title: 'Tabs', type: TabsPageComponent},
-                {view: 'textarea', title: 'Textarea', type: TextareaPageComponent},
-                {view: 'toast', title: 'Toast', type: ToastPageComponent},
-                {view: 'toggle', title: 'Toggle', type: TogglePageComponent},
-                {view: 'tooltip', title: 'Tooltip', type: TooltipPageComponent},
+                { view: 'avatar', title: 'Avatar', type: AvatarPageComponent },
+                { view: 'button', title: 'Button', type: ButtonPageComponent },
+                { view: 'datetime', title: 'Date/time', type: DateTimePageComponent },
+                { view: 'dropdown', title: 'Dropdown', type: DropdownPageComponent },
+                { view: 'popup', title: 'Popup', type: PopupPageComponent },
+                { view: 'tables', title: 'Tables', type: TablePageComponent },
+                { view: 'tabs', title: 'Tabs', type: TabsPageComponent },
+                { view: 'toast', title: 'Toast', type: ToastPageComponent },
+                { view: 'tooltip', title: 'Tooltip', type: TooltipPageComponent },
+            ],
+        },
+        {
+            title: 'Form elements',
+            pages: [
+                { view: 'checkbox', title: 'Checkbox', type: CheckboxPageComponent },
+                { view: 'input', title: 'Input', type: InputPageComponent },
+                { view: 'select', title: 'Select', type: SelectPageComponent },
+                { view: 'textarea', title: 'Textarea', type: TextareaPageComponent },
+                { view: 'toggle', title: 'Toggle', type: TogglePageComponent },
             ],
         },
         {
             title: 'Style',
-            pages: [
-                {view: 'grid', title: 'Grid', type: GridPageComponent},
-            ],
+            pages: [{ view: 'grid', title: 'Grid', type: GridPageComponent }],
         },
         {
             title: 'Utilities',
-            pages: [
-                {view: 'translate', title: 'Translate', type: TranslatePageComponent},
-            ],
+            pages: [{ view: 'translate', title: 'Translate', type: TranslatePageComponent }],
         },
     ];
 
     constructor(private traverser: Traverser) {
         traverser.addView('view', '', WelcomePageComponent);
-        this.menu.forEach(section => section.pages.forEach(page => traverser.addView(page.view, '', page.type)));
+        this.menu.forEach((section) => section.pages.forEach((page) => traverser.addView(page.view, '', page.type)));
     }
 }
---
+++ projects/demo/src/app/demo/pages/palette-page/palette-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'palette-page.component.html',
+    styleUrls: ['palette-page.component.scss'],
+})
+export class PalettePageComponent {}
