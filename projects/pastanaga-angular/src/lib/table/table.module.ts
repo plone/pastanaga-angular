3394443ce42590feead9f77a67fcf2459a03a0fb
mpellerin <mathilde@onna.com>
2020-08-25 10:54:45 +0200

    Table: one folder by component

--- projects/pastanaga-angular/src/lib/table/table.module.ts
+++ projects/pastanaga-angular/src/lib/table/table.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { TableCellComponent } from './table-cell.component';
+import { TableCellComponent } from './table-cell/table-cell.component';
 import { TableComponent } from './table.component';
 import { TableHeaderDirective } from './table.directives';
-import { TableRowComponent } from './table-row.component';
-import { TableRowHeaderComponent } from './table-row-header.component';
+import { TableRowComponent } from './table-row/table-row.component';
+import { TableRowHeaderComponent } from './table-row-header/table-row-header.component';
 
 @NgModule({
     imports: [CommonModule],
