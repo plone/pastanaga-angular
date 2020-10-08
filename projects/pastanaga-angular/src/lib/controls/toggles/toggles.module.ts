d5f78c09a64f788fda2cdc5c4ebc70197e136e5c
mpellerin <mathilde@onna.com>
2020-10-08 18:34:31 +0200

    Remove deprecated checkbox component

--- projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
+++ projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DeprecatedCheckboxComponent } from './checkbox/deprecated/deprecated-checkbox.component';
 import { DeprecatedToggleComponent } from './toggle/deprecated-toggle.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
-    declarations: [DeprecatedCheckboxComponent, DeprecatedToggleComponent, CheckboxComponent],
+    declarations: [DeprecatedToggleComponent, CheckboxComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [DeprecatedCheckboxComponent, DeprecatedToggleComponent, CheckboxComponent],
+    exports: [DeprecatedToggleComponent, CheckboxComponent],
 })
 export class PaTogglesModule {}
