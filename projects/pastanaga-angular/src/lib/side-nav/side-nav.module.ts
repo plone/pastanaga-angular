910ac51c324712a29c7654ac6956e0a43eac0703
Nicolas iglesias <niglesiaszingoni@gmail.com>
2020-11-11 17:05:08 +0100

    Added some improvements to the sidenav

--- projects/pastanaga-angular/src/lib/side-nav/side-nav.module.ts
+++ projects/pastanaga-angular/src/lib/side-nav/side-nav.module.ts
@@ -5,9 +5,10 @@ import { SideNavItemComponent } from './side-nav-item.component';
 import { SideNavContentDirective, SideNavFooterDirective, SideNavHeaderDirective } from './side-nav.directive';
 import { PaButtonModule } from '../button/button.module';
 import { PaFocusableModule } from '../focusable/focusable.module';
+import { PaIconModule } from '../icon/icon.module';
 
 @NgModule({
-    imports: [CommonModule, PaButtonModule, PaFocusableModule],
+    imports: [CommonModule, PaButtonModule, PaFocusableModule, PaIconModule],
     declarations: [
         SideNavComponent,
         SideNavItemComponent,
