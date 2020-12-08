3d4c6a260ff3583b0b78ac33d65768434dcd8472
Eric BREHAULT <ebrehault@gmail.com>
2020-12-08 17:21:53 +0100

    Fix npm release

--- ghpages.sh
+++ ghpages.sh
@@ -4,7 +4,7 @@ REPO_PATH=$PWD
 pushd $HOME
 git config --global user.email "github@github.com";	
 git config --global user.name "GitHub Actions";
-git clone --branch=gh-pages https://${GITHUB_TOKEN}@github.com/plone/pastanaga-angular publish 2>&1 > /dev/null
+git clone --branch=gh-pages git@github.com:plone/pastanaga-angular publish 2>&1 > /dev/null
 cd publish
 rm -rf v2
 cp -r $REPO_PATH/dist/demo/v2 .
