62387ac482e04986f06a1856a189d3f6a731815c
Eric BREHAULT <ebrehault@gmail.com>
2021-01-04 17:56:08 +0100

    fix npm release

--- tag-version.sh
+++ tag-version.sh
@@ -4,6 +4,7 @@ git config --global user.email "github@github.com";
 git config --global user.name "GitHub Actions";
 export PACKAGE_VERSION=$(npm run get_version | tail -n 1);
 if ! [ $(git tag -l "$PACKAGE_VERSION") ]; then
+    npm install && npm run build && cd dist/pastanaga-angular && npm publish && cd -;
     echo "TAGGING $PACKAGE_VERSION...";
     git tag $PACKAGE_VERSION;
     git push --tags;
