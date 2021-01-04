d253777fe6a6cd6a96cc6a5ffd8b91882f61ad85
Eric BREHAULT <ebrehault@gmail.com>
2021-01-04 10:26:41 +0100

    Use GitHub Actions

--- tag-version.sh
+++ tag-version.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-git config --global user.email "build@travis-ci.com";
-git config --global user.name "Travis CI";
+git config --global user.email "github@github.com";	
+git config --global user.name "GitHub Actions";
 export PACKAGE_VERSION=$(npm run get_version | tail -n 1);
 if ! [ $(git tag -l "$PACKAGE_VERSION") ]; then
     echo "TAGGING $PACKAGE_VERSION...";
-    export TRAVIS_TAG=$PACKAGE_VERSION;
     git tag $PACKAGE_VERSION;
+    git push --tags;
 fi
