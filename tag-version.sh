43a4e9b776a6524f82bc5c8d1ae033125017c3ad
Eric BREHAULT <ebrehault@gmail.com>
2021-01-05 14:29:59 +0100

    Fix GH pages update

--- tag-version.sh
+++ tag-version.sh
@@ -3,8 +3,9 @@
 git config --global user.email "github@github.com";	
 git config --global user.name "GitHub Actions";
 export PACKAGE_VERSION=$(npm run get_version | tail -n 1);
+echo "PACKAGE VERSION: $PACKAGE_VERSION"
+echo "Search in existing… $(git tag -l "$PACKAGE_VERSION")"
 if ! [ $(git tag -l "$PACKAGE_VERSION") ]; then
-    npm install && npm run build && cd dist/pastanaga-angular && npm publish && cd -;
     echo "TAGGING $PACKAGE_VERSION...";
     git tag $PACKAGE_VERSION;
     git push --tags;
