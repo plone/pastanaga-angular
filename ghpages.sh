5956921784f138a9705de0c3adecdfd1fe25e2fa
Eric BREHAULT <ebrehault@gmail.com>
2021-01-05 20:11:53 +0100

    Fix gh pages

--- ghpages.sh
+++ ghpages.sh
@@ -7,7 +7,7 @@ git config --global user.name "GitHub Actions";
 git clone --branch=gh-pages https://${GH_ACTIONS_TOKEN}@github.com/plone/pastanaga-angular publish 2>&1 > /dev/null
 cd publish
 rm -rf v1
-cp -r $REPO_PATH/dist/demo/v1 .
+cp -r $REPO_PATH/dist/pastanaga-app/v1 .
 git add .
 git commit -m "Update demo v1"
 git push -fq origin gh-pages 2>&1 > /dev/null
