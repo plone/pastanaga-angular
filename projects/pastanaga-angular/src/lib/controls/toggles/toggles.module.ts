6e38ba4be20825b7a6abbce1a46d356f3a24804a
julien carret <jul.carret@gmail.com>
2021-01-12 10:28:40 +0100

    added error display for checkbox-component

--- projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
+++ projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
@@ -4,10 +4,11 @@ import { DeprecatedToggleComponent } from './toggle/deprecated-toggle.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { PaFocusableModule } from '../../focusable/focusable.module';
+import { PaFormFieldModule } from '../form-field/form-field.module';
 
 @NgModule({
     declarations: [DeprecatedToggleComponent, CheckboxComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, PaFocusableModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, PaFocusableModule, PaFormFieldModule],
     exports: [DeprecatedToggleComponent, CheckboxComponent],
 })
 export class PaTogglesModule {}
