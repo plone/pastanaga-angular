7d20dfa1db573a15004bf42402f89d4a5e01a11e
julien carret <jul.carret@gmail.com>
2021-01-12 14:07:39 +0100

    cleaned and add coverage to select-options.component

--- projects/pastanaga-angular/src/lib/controls/textfield/select/select-options/select-options.component.ts
+++ projects/pastanaga-angular/src/lib/controls/textfield/select/select-options/select-options.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { OptionHeaderModel, OptionModel, OptionSeparator } from '../../../control.model';
 
 @Component({
     selector: 'pa-select-options',
     templateUrl: './select-options.component.html',
-    styleUrls: ['./select-options.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SelectOptionsComponent implements OnInit {
+export class SelectOptionsComponent {
     @Input() options: (OptionModel | OptionSeparator | OptionHeaderModel)[] = [];
     @Output() optionSelected = new EventEmitter<OptionModel>();
-    constructor() {}
-
-    ngOnInit(): void {}
 
     selectOption(option: OptionModel) {
         this.optionSelected.emit(option);
