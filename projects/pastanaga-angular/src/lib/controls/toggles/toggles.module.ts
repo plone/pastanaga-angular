4c3c0fa8866361fba454974597dd76bddd21296e
Eric BREHAULT <ebrehault@gmail.com>
2021-02-23 18:01:38 +0100

    clean up deprecated components

--- projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
+++ projects/pastanaga-angular/src/lib/controls/toggles/toggles.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DeprecatedToggleComponent } from './toggle/deprecated-toggle.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { PaFocusableModule } from '../../focusable/focusable.module';
@@ -8,8 +7,8 @@ import { PaFormFieldModule } from '../form-field/form-field.module';
 import { ToggleComponent } from './toggle/toggle.component';
 
 @NgModule({
-    declarations: [DeprecatedToggleComponent, CheckboxComponent, ToggleComponent],
+    declarations: [CheckboxComponent, ToggleComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule, PaFocusableModule, PaFormFieldModule],
-    exports: [DeprecatedToggleComponent, CheckboxComponent, ToggleComponent],
+    exports: [CheckboxComponent, ToggleComponent],
 })
 export class PaTogglesModule {}
