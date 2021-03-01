34881565f1bf6e651ae24106dbfd7fc3ecd804c0
julien carret <jul.carret@gmail.com>
2021-03-01 14:13:12 +0100

    fixed missing exports

--- projects/pastanaga/src/public_api.ts
+++ projects/pastanaga/src/public_api.ts
@@ -3,98 +3,110 @@
  */
 
 export { PastanagaService } from './lib/pastanaga.service';
-export * from './lib/badge/badge.module';
-export * from './lib/badge/badge.model';
-export * from './lib/badge/badge.component';
+export * from './lib/test.utils';
+export * from './lib/keycodes.constant';
 
-export * from './lib/button/button.module';
-export * from './lib/button/button.component';
+export * from './lib/auto-focus/auto-focus.directive';
+export * from './lib/auto-focus/auto-focus.module';
+
+export * from './lib/avatar/avatar.component';
+export * from './lib/avatar/avatar.model';
+export * from './lib/avatar/avatar.module';
+
+export * from './lib/badge/badge.component';
+export * from './lib/badge/badge.model';
+export * from './lib/badge/badge.module';
+
 export * from './lib/button/button-base';
+export * from './lib/button/button.component';
+export * from './lib/button/button.module';
+
+export * from './lib/calendar/calendar.component';
+export * from './lib/calendar/calendar.model';
+export * from './lib/calendar/calendar.module';
+export * from './lib/calendar/calendar.service';
+export * from './lib/calendar/date-input.component';
+export * from './lib/calendar/date-picker.component';
 
 export * from './lib/common/utils';
-export * from './lib/test.utils';
 
-export * from './lib/controls/controls.module';
 export * from './lib/controls/control.model';
+export * from './lib/controls/controls.module';
+export * from './lib/controls/toggle.model';
 export * from './lib/controls/checkbox/checkbox.component';
+export * from './lib/controls/checkbox-group/checkbox-group.component';
 export * from './lib/controls/checkbox-tree/checkbox-tree.component';
 export * from './lib/controls/checkbox-tree/checkbox-tree.test-data';
-export * from './lib/controls/checkbox-group/checkbox-group.component';
-export * from './lib/controls/filtered-checkbox-group/filtered-checkbox-group.component';
 export * from './lib/controls/double-slider/double-slider.component';
+export * from './lib/controls/filtered-checkbox-group/directory.component';
+export * from './lib/controls/filtered-checkbox-group/filtered-checkbox-group.component';
 export * from './lib/controls/slider/slider.component';
-export * from './lib/controls/toggle.model';
 export * from './lib/controls/toggle/toggle.component';
 export * from './lib/controls/toggle-group/toggle-group.component';
 
-export * from './lib/progress/progress.module';
-export * from './lib/progress/progress.component';
+export * from './lib/dialog/base-dialog.component';
+export * from './lib/dialog/basic-confirm-dialog.component';
+export * from './lib/dialog/confirm-dialog.component';
+export * from './lib/dialog/dialog.component';
+export * from './lib/dialog/dialog.directives';
+export * from './lib/dialog/dialog.model';
+export * from './lib/dialog/dialog.module';
+export * from './lib/dialog/dialog.service';
 
-export * from './lib/svg/svg.module';
-export * from './lib/svg/icon.component';
-export * from './lib/svg/svg-loader';
-export * from './lib/svg/svg-loader.interface';
-
-export * from './lib/textfield/textfield.module';
-export * from './lib/textfield/textfield.common';
-export * from './lib/textfield/input.component';
-export * from './lib/textfield/select.component';
-
-export * from './lib/toast/toast.module';
-export * from './lib/toast/toast.component';
-export * from './lib/toast/toast.model';
-export * from './lib/toast/toast.service';
-
-export * from './lib/tooltip/tooltip.module';
-export * from './lib/tooltip/tooltip.component';
-export * from './lib/tooltip/tooltip.directive';
+export * from './lib/dropdown/module';
+export * from './lib/dropdown/dropdown.component';
+export * from './lib/dropdown/dropdown-checkbox.component';
+export * from './lib/dropdown/dropdown-item.component';
+export * from './lib/dropdown/dropdown-section.component';
+export * from './lib/dropdown/dropdown-separator.component';
 
 export * from './lib/expand/expand.component';
 export * from './lib/expand/expand.module';
+export * from './lib/expand/expand-list.component';
 
 export * from './lib/pagination/pagination.component';
 export * from './lib/pagination/pagination.module';
 
-export * from './lib/translate/translate.module';
-export * from './lib/translate/translate.pipe';
-export * from './lib/translate/translate.directive';
-export * from './lib/translate/translate.model';
-export * from './lib/translate/translate.utils';
-export * from './lib/translate/i18n/en';
+export * from './lib/popup/popup.component';
+export * from './lib/popup/popup.directive';
+export * from './lib/popup/popup.module';
+export * from './lib/popup/popup.service';
+
+export * from './lib/progress/progress.component';
+export * from './lib/progress/progress.module';
+export * from './lib/progress/progress-circle.component';
+export * from './lib/progress/spinner.component';
 
 export * from './lib/sidebar/module';
 export * from './lib/sidebar/sidebar.component';
 export * from './lib/sidebar/sidebar.service';
 
-export * from './lib/auto-focus/auto-focus.module';
-export * from './lib/auto-focus/auto-focus.directive';
+export * from './lib/svg/icon.component';
+export * from './lib/svg/svg.module';
+export * from './lib/svg/svg-loader.interface';
+export * from './lib/svg/svg-loader';
 
-export * from './lib/calendar/calendar.module';
-export * from './lib/calendar/calendar.model';
-export * from './lib/calendar/calendar.component';
-export * from './lib/calendar/calendar.service';
-export * from './lib/calendar/date-picker.component';
+export * from './lib/textfield/input-icon/input-icon.component';
+export * from './lib/textfield/password-input/password-input.component';
+export * from './lib/textfield/password-input/password-rule';
+export * from './lib/textfield/input.component';
+export * from './lib/textfield/select.component';
+export * from './lib/textfield/textarea.component';
+export * from './lib/textfield/textfield.common';
+export * from './lib/textfield/textfield.module';
 
-export * from './lib/popup/popup.module';
-export * from './lib/popup/popup.directive';
-export * from './lib/popup/popup.component';
-export * from './lib/popup/popup.service';
+export * from './lib/toast/toast.component';
+export * from './lib/toast/toast.model';
+export * from './lib/toast/toast.module';
+export * from './lib/toast/toast.service';
 
-export * from './lib/dropdown/module';
-export * from './lib/dropdown/dropdown-item.component';
-export * from './lib/dropdown/dropdown-checkbox.component';
-export * from './lib/dropdown/dropdown-section.component';
-export * from './lib/dropdown/dropdown-separator.component';
+export * from './lib/tooltip/tooltip.component';
+export * from './lib/tooltip/tooltip.directive';
+export * from './lib/tooltip/tooltip.module';
 
-export * from './lib/avatar/avatar.model';
-export * from './lib/avatar/avatar.component';
-export * from './lib/avatar/avatar.module';
-
-export * from './lib/dialog/base-dialog.component';
-export * from './lib/dialog/dialog.component';
-export * from './lib/dialog/dialog.model';
-export * from './lib/dialog/dialog.module';
-export * from './lib/dialog/dialog.directives';
-export * from './lib/dialog/dialog.service';
-export * from './lib/dialog/confirm-dialog.component';
-export * from './lib/dialog/basic-confirm-dialog.component';
+export * from './lib/translate/i18n/en';
+export * from './lib/translate/translate.directive';
+export * from './lib/translate/translate.model';
+export * from './lib/translate/translate.module';
+export * from './lib/translate/translate.pipe';
+export * from './lib/translate/translate.utils';
