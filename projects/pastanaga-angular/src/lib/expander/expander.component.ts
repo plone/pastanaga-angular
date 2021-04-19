a4ffd3c68bcca8954b6f18d7c1f72de97a622cfc
Mat Pellerin <mat@onna.com>
2021-04-19 12:09:25 +0200

    Fix expander

--- projects/pastanaga-angular/src/lib/expander/expander.component.ts
+++ projects/pastanaga-angular/src/lib/expander/expander.component.ts
@@ -1,5 +1,16 @@
-import { Component, ChangeDetectionStrategy, ElementRef, AfterViewInit, ContentChild, Input } from '@angular/core';
+import {
+    Component,
+    ChangeDetectionStrategy,
+    ElementRef,
+    AfterViewInit,
+    ContentChild,
+    Input,
+    ChangeDetectorRef,
+} from '@angular/core';
 import { ExpanderBodyDirective } from './expander.directive';
+import { markForCheck } from '../common';
+
+export const transitionDuration = 160;
 
 @Component({
     selector: 'pa-expander',
@@ -15,15 +26,30 @@ export class ExpanderComponent implements AfterViewInit {
     @ContentChild(ExpanderBodyDirective, { read: ElementRef }) expanderContent?: ElementRef;
 
     expanded = true;
+    contentHidden = false;
 
-    constructor(private elementRef: ElementRef) {}
+    constructor(private elementRef: ElementRef, private cdr: ChangeDetectorRef) {}
 
     ngAfterViewInit() {
         this.updateContentHeight();
     }
 
     toggleExpand() {
-        this.expanded = !this.expanded;
+        // when expanded, we collapse directly and hide content after the transition delay
+        if (this.expanded) {
+            this.expanded = false;
+            setTimeout(() => {
+                this.contentHidden = true;
+                markForCheck(this.cdr);
+            }, transitionDuration);
+        } else {
+            // when collapsed, we remove display: none before expand the panel so the animation is visible
+            this.contentHidden = false;
+            setTimeout(() => {
+                this.expanded = true;
+                markForCheck(this.cdr);
+            }, 0);
+        }
     }
 
     private updateContentHeight() {
