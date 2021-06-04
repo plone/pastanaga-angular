2642a8a49e1797dc03e39751692f95188650b59f
Mat Pellerin <mat@onna.com>
2021-06-04 09:31:44 +0200

    Minimal unit test

--- projects/pastanaga-angular/src/lib/scroll/infinite-scroll/infinite-scroll.component.spec.ts
+++ projects/pastanaga-angular/src/lib/scroll/infinite-scroll/infinite-scroll.component.spec.ts
@@ -1,24 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfiniteScrollComponent } from './infinite-scroll.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 
 describe('VirtualScrollComponent', () => {
-    let component: InfiniteScrollComponent;
-    let fixture: ComponentFixture<InfiniteScrollComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [InfiniteScrollComponent],
-        }).compileComponents();
+    const createComponent = createComponentFactory({
+        component: InfiniteScrollComponent,
+        detectChanges: false,
     });
+    let component: InfiniteScrollComponent;
+    let spectator: Spectator<InfiniteScrollComponent>;
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(InfiniteScrollComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        spectator = createComponent();
+        component = spectator.component;
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should have two anchors in the dom', () => {
+        expect(spectator.query('[qa="mid-anchor"]')).toBeTruthy();
+        expect(spectator.query('[qa="bottom-anchor"]')).toBeTruthy();
     });
 });
