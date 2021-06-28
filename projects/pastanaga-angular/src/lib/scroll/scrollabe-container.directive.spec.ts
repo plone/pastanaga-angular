5816cedf876fd77f59ef751d012d99110474295b
Mat Pellerin <mat@onna.com>
2021-06-28 15:36:59 +0200

    Improve scrollbar style (#478)

--- projects/pastanaga-angular/src/lib/scroll/scrollabe-container.directive.spec.ts
+++ projects/pastanaga-angular/src/lib/scroll/scrollabe-container.directive.spec.ts
@@ -1,10 +1,6 @@
 import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator/jest';
 import { fakeAsync, flush, tick } from '@angular/core/testing';
-import {
-    SCROLL_INACTIVE_FADING_THRESHOLD,
-    SCROLL_INACTIVE_HIDING_THRESHOLD,
-    ScrollableContainerDirective,
-} from './scrollabe-container.directive';
+import { SCROLL_INACTIVE_HIDING_THRESHOLD, ScrollableContainerDirective } from './scrollabe-container.directive';
 import { TRANSITION_DURATION } from '../common';
 
 describe('ScrollableContainerDirective', () => {
@@ -18,43 +14,16 @@ describe('ScrollableContainerDirective', () => {
         expect(spectator.element).toHaveClass('pa-scrollable');
     }));
 
-    it('should add pa-scrolling class on scroll then pa-fading-scrollbar then remove all', fakeAsync(() => {
+    it('should add pa-scrolling class on scroll then remove it', fakeAsync(() => {
         spectator = createDirective(`<div paScrollableContainer>`);
         spectator.detectChanges();
         flush();
         expect(spectator.element).not.toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
         spectator.dispatchTouchEvent(spectator.element, 'scroll');
         spectator.dispatchTouchEvent(spectator.element, 'scroll');
         tick(TRANSITION_DURATION.fast);
         expect(spectator.element).toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
-        tick(SCROLL_INACTIVE_FADING_THRESHOLD - TRANSITION_DURATION.fast);
-        expect(spectator.element).toHaveClass('pa-scrolling');
-        expect(spectator.element).toHaveClass('pa-fading-scrollbar');
-        tick(SCROLL_INACTIVE_HIDING_THRESHOLD - SCROLL_INACTIVE_FADING_THRESHOLD);
-        expect(spectator.element).not.toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
-    }));
-
-    it('should revert pa-fading-scrollbar when user scroll again', fakeAsync(() => {
-        spectator = createDirective(`<div paScrollableContainer>`);
-        spectator.detectChanges();
-        flush();
-        expect(spectator.element).not.toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
-        spectator.dispatchTouchEvent(spectator.element, 'scroll');
-        spectator.dispatchTouchEvent(spectator.element, 'scroll');
-        tick(TRANSITION_DURATION.fast);
-        expect(spectator.element).toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
-        tick(SCROLL_INACTIVE_FADING_THRESHOLD - TRANSITION_DURATION.fast);
-        expect(spectator.element).toHaveClass('pa-scrolling');
-        expect(spectator.element).toHaveClass('pa-fading-scrollbar');
-        spectator.dispatchTouchEvent(spectator.element, 'scroll');
-        tick(TRANSITION_DURATION.fast);
-        expect(spectator.element).toHaveClass('pa-scrolling');
-        expect(spectator.element).not.toHaveClass('pa-fading-scrollbar');
         tick(SCROLL_INACTIVE_HIDING_THRESHOLD);
+        expect(spectator.element).not.toHaveClass('pa-scrolling');
     }));
 });
--- projects/pastanaga-angular/src/lib/scroll/scrollabe-container.directive.ts
+++ projects/pastanaga-angular/src/lib/scroll/scrollabe-container.directive.ts
@@ -12,11 +12,11 @@ import {
     Renderer2,
 } from '@angular/core';
 import { fromEvent, merge, Subject } from 'rxjs';
-import { debounceTime, mapTo, takeUntil, throttleTime } from 'rxjs/operators';
+import { debounceTime, mapTo, takeUntil, tap, throttleTime } from 'rxjs/operators';
 import { markForCheck, TRANSITION_DURATION } from '../common';
 import { DOCUMENT } from '@angular/common';
+import { PopupService } from '../popup/popup.service';
 
-export const SCROLL_INACTIVE_FADING_THRESHOLD = 1500;
 export const SCROLL_INACTIVE_HIDING_THRESHOLD = 3000;
 
 @Directive({
@@ -27,7 +27,6 @@ export class ScrollableContainerDirective implements OnInit, AfterContentInit, O
 
     private _terminator = new Subject();
     private _scrollActive = false;
-    private _scrollFading = false;
 
     constructor(
         @Inject(DOCUMENT) private document: Document,
@@ -35,6 +34,7 @@ export class ScrollableContainerDirective implements OnInit, AfterContentInit, O
         private renderer: Renderer2,
         private ngZone: NgZone,
         private cdr: ChangeDetectorRef,
+        private popupService: PopupService,
     ) {}
 
     ngOnInit() {
@@ -45,17 +45,14 @@ export class ScrollableContainerDirective implements OnInit, AfterContentInit, O
                     this.resize.emit();
                 });
 
-            const scroll$ = fromEvent(this.element.nativeElement, 'scroll').pipe(takeUntil(this._terminator));
+            const scroll$ = fromEvent(this.element.nativeElement, 'scroll').pipe(
+                tap(() => this.popupService.closeAllPopups.next()),
+                takeUntil(this._terminator),
+            );
 
             const showScrollbar$ = scroll$.pipe(
                 throttleTime(TRANSITION_DURATION.fast),
                 mapTo('show'),
-                takeUntil(this._terminator),
-            );
-
-            const fadeScrollbar$ = scroll$.pipe(
-                debounceTime(SCROLL_INACTIVE_FADING_THRESHOLD),
-                mapTo('fade'),
                 takeUntil(this._terminator),
             );
 
@@ -64,13 +61,10 @@ export class ScrollableContainerDirective implements OnInit, AfterContentInit, O
                 mapTo('hide'),
                 takeUntil(this._terminator),
             );
-            merge(showScrollbar$, fadeScrollbar$, hideScrollbar$).subscribe((action) => {
+            merge(showScrollbar$, hideScrollbar$).subscribe((action) => {
                 switch (action) {
                     case 'show':
                         this.showScrollbar();
-                        break;
-                    case 'fade':
-                        this.fadeScrollbar();
                         break;
                     case 'hide':
                         this.hideScrollbar();
@@ -103,25 +97,11 @@ export class ScrollableContainerDirective implements OnInit, AfterContentInit, O
             this._scrollActive = true;
             markForCheck(this.cdr);
         }
-        if (this._scrollFading) {
-            this.renderer.removeClass(this.element.nativeElement, 'pa-fading-scrollbar');
-            this._scrollFading = false;
-            markForCheck(this.cdr);
-        }
-    }
-
-    fadeScrollbar() {
-        if (!this._scrollFading) {
-            this.renderer.addClass(this.element.nativeElement, 'pa-fading-scrollbar');
-            this._scrollFading = true;
-            markForCheck(this.cdr);
-        }
     }
 
     hideScrollbar() {
         if (this._scrollActive) {
             this.renderer.removeClass(this.element.nativeElement, 'pa-scrolling');
-            this.renderer.removeClass(this.element.nativeElement, 'pa-fading-scrollbar');
             this._scrollActive = false;
             markForCheck(this.cdr);
         }
