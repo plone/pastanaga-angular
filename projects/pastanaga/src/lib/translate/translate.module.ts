34bb3a565d24b5d11cb1479b3958b6c49ff67712
Roger Boixader <rboixaderg@gmail.com>
2021-11-25 10:56:10 +0100

    fix: delete service from module

--- projects/pastanaga/src/lib/translate/translate.module.ts
+++ projects/pastanaga/src/lib/translate/translate.module.ts
@@ -2,7 +2,6 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe, TRANSLATIONS } from './translate.pipe';
 import { TranslateDirective } from './translate.directive';
-import { TranslateService } from './translate.service';
 import { Translation } from './translate.model';
 import { mergeTranslations } from './translate.utils';
 
@@ -10,8 +9,8 @@ const _TRANSLATIONS: Translation = {};
 
 @NgModule({
     imports: [CommonModule],
-    exports: [TranslatePipe, TranslateDirective, TranslateService],
-    declarations: [TranslatePipe, TranslateDirective, TranslateService],
+    exports: [TranslatePipe, TranslateDirective],
+    declarations: [TranslatePipe, TranslateDirective],
     providers: [TranslatePipe],
 })
 export class TranslateModule {
