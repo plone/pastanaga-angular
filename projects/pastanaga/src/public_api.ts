585e0c3ade70d1eb615c413ed0bc5bbe177ac0fc
Roger Boixader <rboixaderg@gmail.com>
2021-11-25 11:03:50 +0100

    feat: export translate service

--- projects/pastanaga/src/public_api.ts
+++ projects/pastanaga/src/public_api.ts
@@ -111,3 +111,4 @@ export * from './lib/translate/translate.model';
 export * from './lib/translate/translate.module';
 export * from './lib/translate/translate.pipe';
 export * from './lib/translate/translate.utils';
+export * from './lib/translate/translate.service';
