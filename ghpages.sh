21b81602285f0c1437a82ebd224864a46162db96
mpellerin42 <mpellerin42@gmail.com>
2022-06-30 10:21:49 +0200

    Deploy v2 demo at the root of gh-pages

--- ghpages.sh
+++ ghpages.sh
@@ -2,13 +2,14 @@
 
 REPO_PATH=$PWD
 pushd $HOME
-git config --global user.email "github@github.com";	
+git config --global user.email "github@github.com";
 git config --global user.name "GitHub Actions";
 git clone --branch=gh-pages https://${GH_ACTIONS_TOKEN}@github.com/plone/pastanaga-angular publish 2>&1 > /dev/null
 cd publish
-rm -rf v2
-cp -r $REPO_PATH/dist/demo/v2 .
+# delete main demo: find and remove all files except current and v1 directories
+find . -maxdepth 1 ! -name v1 ! -name . -exec rm -rv {} \;
+cp -r $REPO_PATH/dist/demo .
 git add .
-git commit -m "Update demo v2"
+git commit -m "Update main demo"
 git push -fq origin gh-pages 2>&1 > /dev/null
 popd
