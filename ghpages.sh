0589343e038eb783f917e3c7bf5a3aa09ec1e326
mpellerin42 <mpellerin42@gmail.com>
2022-07-01 11:36:19 +0200

    Fix demo deployment
    Don't remove .git folder cleaning up the previous demo

--- ghpages.sh
+++ ghpages.sh
@@ -6,8 +6,8 @@ git config --global user.email "github@github.com";
 git config --global user.name "GitHub Actions";
 git clone --branch=gh-pages https://${GH_ACTIONS_TOKEN}@github.com/plone/pastanaga-angular publish 2>&1 > /dev/null
 cd publish
-# delete main demo: find and remove all files except current and v1 directories
-find . -maxdepth 1 ! -name v1 ! -name . -exec rm -rv {} \;
+# delete main demo: find and remove all files except current, v1 and .git directories
+find . -maxdepth 1 ! -name v1 ! -name .git ! -name . -exec rm -rv {} \;
 cp -r $REPO_PATH/dist/demo .
 git add .
 git commit -m "Update main demo"
