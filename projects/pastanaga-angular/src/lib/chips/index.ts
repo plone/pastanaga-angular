a937f231f7f59d91052f8ef29973b731dfa33579
Mat Pellerin <mpellerin42@gmail.com>
2023-12-11 12:04:36 +0100

    Add  in index.ts to fix the library build (#677)

--- projects/pastanaga-angular/src/lib/chips/index.ts
+++ projects/pastanaga-angular/src/lib/chips/index.ts
@@ -1,5 +1,6 @@
 export * from './base-chip';
 export * from './chips.module';
+export * from './chip/chip.component';
 export * from './chip-closeable/chip-closeable.component';
 export * from './chip-expandable/chip-expandable.component';
 export * from './chip-selectionable/chip-selectionable.component';
