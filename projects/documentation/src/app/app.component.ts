5b0ea36de60d5492dffea1e112f0d40a2db46880
mpellerin42 <mpellerin42@gmail.com>
2025-03-10 10:10:58 +0100

    Setup documentation app

---
+++ projects/documentation/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'doc-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'documentation';
+}
