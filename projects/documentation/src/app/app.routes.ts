e1eef8d16c8ef9b6dde205cf9dab8b17dccede5b
mpellerin42 <mpellerin42@gmail.com>
2025-03-11 11:14:11 +0100

    Move palette in its own component accessible through a route

--- projects/documentation/src/app/app.routes.ts
+++ projects/documentation/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaletteComponent } from './pages/palette/palette.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'palette', component: PaletteComponent },
+];
